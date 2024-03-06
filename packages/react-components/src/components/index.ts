b5642ac3c80473d4662913b23abe2f76b17bb461
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-03-06 14:05:37 -0800

    Add Header component

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -1,6 +1,7 @@
 import "@bcgov/design-tokens/css/variables.css";
 
 export { default as Button } from "./Button";
+export { default as Header, SvgBcLogo } from "./Header";
 export { default as Select } from "./Select";
 export { default as Tag } from "./Tag";
 export { default as TagGroup } from "./TagGroup";
