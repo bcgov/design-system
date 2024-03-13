b33c3d559817d9aa643cb42300a79a43059be25b
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-13 15:26:49 -0700

    Update Storybook branding (#299)
    * set up basic storybook theming
    
    * add BC Sans and set as default font for theme
    
    * add design tokens and configure colours
    
    * correct typo
    
    * debugging tokens
    
    * fixing broken logo link
    
    * remove redundant files
    
    * tweak colour settings
    
    * fix broken asset path
    
    * configure UI colours
    
    * configure text colours
    
    * trying to fix broken asset path again
    
    * still debugging broken asset path
    
    * configure toolbar colours
    
    * fix typo
    
    * configure form styling
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Tyler Krys <Tyler.Krys@gov.bc.ca>

---
+++ packages/react-components/.storybook/bcdsTheme.js
@@ -0,0 +1,41 @@
+import { create } from "@storybook/theming/create";
+import "@bcgov/bc-sans/css/BC_Sans.css";
+import * as tokens from "@bcgov/design-tokens/js/variables.js";
+
+export default create({
+  // Typography
+  fontBase: '"BC Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Colour
+  colorPrimary: tokens.surfaceBrandGold60,
+  colorSecondary: tokens.surfaceBrandBlue100,
+
+  // UI
+  appBg: tokens.surfaceBackgroundLight,
+  appContentBg: tokens.surfaceBackgroundWhite,
+  appPreviewBg: tokens.surfaceBackgroundWhite,
+  appBorderColor: tokens.surfaceBorderMedium,
+  appBorderRadius: tokens.surfaceBorderRadiusMedium,
+
+  // Text colors
+  textColor: tokens.typographyColorPrimary,
+  textInverseColor: tokens.typographyColorPrimaryInvert,
+
+  // Toolbar default and active colors
+  barTextColor: tokens.typographyColorSecondary,
+  barSelectedColor: tokens.surfacePrimaryActive,
+  barHoverColor: tokens.surfacePrimaryHover,
+  barBg: tokens.surfaceBackgroundLight,
+
+  // Form colors
+  inputBg: tokens.surfaceBackgroundLight,
+  inputBorder: tokens.surfaceBorderLight,
+  inputTextColor: tokens.typographyColorSecondary,
+  inputBorderRadius: tokens.surfaceBorderRadiusMedium,
+
+  base: "light",
+  brandTitle: "B.C. Design System",
+  brandUrl: "https://gov.bc.ca/designsystem",
+  brandImage: "../src/assets/BCID_H_rgb_pos.svg",
+});
---
+++ packages/react-components/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import bcdsTheme from "./bcdsTheme";
+
+addons.setConfig({
+  theme: bcdsTheme,
+});
