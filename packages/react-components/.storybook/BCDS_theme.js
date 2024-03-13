72819c8ec62fe90ac1e05c4591578e9b7a6d9409
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-13 13:29:50 -0700

    set up basic storybook theming

---
+++ packages/react-components/.storybook/BCDS_theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'B.C. Design System',
+  brandUrl: 'https://gov.bc.ca/designsystem',
+  brandImage: 'packages/react-components/src/assets/BCID_H_rgb_pos.svg',
+});
---
+++ packages/react-components/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import BCDS_theme from './BCDS_theme';
+
+addons.setConfig({
+  theme: BCDS_theme,
+});
