04bd601049cf4cfcd1c7f9a68d657d737d9065e4
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-13 13:34:32 -0700

    add BC Sans and set as default font for theme

--- packages/react-components/.storybook/BCDS_theme.js
+++ packages/react-components/.storybook/BCDS_theme.js
@@ -1,6 +1,12 @@
 import { create } from '@storybook/theming/create';
+import '@bcgov/bc-sans/css/BC_Sans.css';
 
 export default create({
+
+  // Typography
+  fontBase: '"BC Sans", sans-serif',
+  fontCode: 'monospace',
+
   base: 'light',
   brandTitle: 'B.C. Design System',
   brandUrl: 'https://gov.bc.ca/designsystem',
