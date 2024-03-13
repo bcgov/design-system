f6f7c4c5fffa072878d875029db3151830a2cafa
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-13 13:40:03 -0700

    add design tokens and configure colours

--- packages/react-components/.storybook/BCDS_theme.js
+++ packages/react-components/.storybook/BCDS_theme.js
@@ -1,11 +1,16 @@
 import { create } from '@storybook/theming/create';
 import '@bcgov/bc-sans/css/BC_Sans.css';
+import "@bcgov/design-tokens/js/variables.js";
 
 export default create({
 
   // Typography
   fontBase: '"BC Sans", sans-serif',
   fontCode: 'monospace',
+
+  // Colour
+  colorPrimary: 'surfaceBrandBlue100',
+  colorSecondary: 'surfaceBrandGold60',
 
   base: 'light',
   brandTitle: 'B.C. Design System',
