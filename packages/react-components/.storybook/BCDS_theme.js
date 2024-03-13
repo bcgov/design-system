dc78c0a41368bb4acbbe1db7218a87ea0e2b528d
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-13 14:45:04 -0700

    configure toolbar colours

--- packages/react-components/.storybook/BCDS_theme.js
+++ packages/react-components/.storybook/BCDS_theme.js
@@ -23,6 +23,12 @@ export default create({
   textColor: tokens.typographyColorPrimary,
   textInverseColor: tokens.typographyColorPrimaryInvert,
 
+  // Toolbar default and active colors
+  barTextColor: tokens.typographyColorSecondary,
+  barSelectedColor: tokens.surfacePrimaryActive,
+  barHoverColor: tokens.surfacePrimaryHover,
+  barBg: tokens.surfaceBackgroundLight,
+
   base: 'light',
   brandTitle: 'B.C. Design System',
   brandUrl: 'https://gov.bc.ca/designsystem',
