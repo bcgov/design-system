b7ed812124664e2da4058383cbe0ddd40902d43d
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-03-14 09:34:35 -0700

    add theme support to component docs pages

--- packages/react-components/.storybook/preview.ts
+++ packages/react-components/.storybook/preview.ts
@@ -1,13 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
 
 import "@bcgov/bc-sans/css/BC_Sans.css";
 import "@bcgov/design-tokens/css/variables.css";
 
 import "./global.css";
+import bcdsTheme from "./bcdsTheme";
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    docs: { themes:bcdsTheme },
     controls: {
       matchers: {
         color: /(background|color)$/i,
