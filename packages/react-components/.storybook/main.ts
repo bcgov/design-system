505e93f384b5ffd84e4348ac1f5a7e8b9fab814e
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-05-31 13:57:53 -0700

    add storysource extension

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-storysource",
   ],
   framework: {
     name: "@storybook/react-vite",
