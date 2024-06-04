7aeff640cd1ec02dcc023d4e8e6bab19da8401a0
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-06-04 16:57:53 -0700

    Add configuration for Storybook test-runner

---
+++ packages/react-components/.storybook/test-runner.ts
@@ -0,0 +1,20 @@
+// .storybook/test-runner.ts
+// From: https://storybook.js.org/docs/writing-tests/accessibility-testing#setup
+
+import type { TestRunnerConfig } from "@storybook/test-runner";
+import { injectAxe, checkA11y } from "axe-playwright";
+
+/*
+ * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
+ * to learn more about the test-runner hooks API.
+ */
+const config: TestRunnerConfig = {
+  async preVisit(page) {
+    await injectAxe(page);
+  },
+  async postVisit(page) {
+    await checkA11y(page, "#storybook-root");
+  },
+};
+
+export default config;
