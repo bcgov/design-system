8660993343977c6ef4426eab41cf79d2672e6b38
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-06-14 13:49:33 -0700

    flesh out default TextField configuration and styling and stage on vite

--- packages/react-components/src/components/TextField/TextField.tsx
+++ packages/react-components/src/components/TextField/TextField.tsx
@@ -1,6 +1,11 @@
 import {
   TextField as ReactAriaTextField,
   TextFieldProps as ReactAriaTextFieldProps,
+  Input,
+  Label,
+  FieldError,
+  Text,
+  ValidationResult,
 } from "react-aria-components";
 
 import "./TextField.css";
@@ -8,16 +13,27 @@ import "./TextField.css";
 export interface TextFieldProps extends ReactAriaTextFieldProps {
   /** Sets size of input field */
   size?: "medium" | "small";
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
 }
 
 export default function TextField({
   size = "medium",
+  label = "Label",
+  description = "Description or helper text",
+  errorMessage = "Error message",
   ...props
 }: TextFieldProps) {
   return (
     <ReactAriaTextField
       className={`bcds-react-aria-TextField ${size}`}
       {...props}
-    />
+    >
+      <Label className="bcds-react-aria-TextField--Label">{label}</Label>
+      <Input className="bcds-react-aria-TextField--Input" />
+      {description && <Text slot="description">{description}</Text>}
+      <FieldError>{errorMessage}</FieldError>
+    </ReactAriaTextField>
   );
 }
