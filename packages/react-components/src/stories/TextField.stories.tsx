8cb5b62ffac6a87d46b543f863299c48508912a3
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-06-17 12:09:20 -0700

    docs page skeleton

--- packages/react-components/src/stories/TextField.stories.tsx
+++ packages/react-components/src/stories/TextField.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { TextField } from "../components";
 
 const meta = {
-  title: "Components/TextField",
+  title: "Components/TextField/TextField",
   component: TextField,
   parameters: {
     layout: "centered",
