6afaefadc5aa8c476a2584e3150174876ab19335
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-06-26 10:41:48 -0700

    fork Text Field component into Text Area, replacing <Input> with <TextArea>

---
+++ packages/react-components/src/stories/TextArea.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { TextArea } from "../components";
+
+const meta = {
+  title: "Components/TextArea",
+  component: TextArea,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    value: {
+      control: { type: "text" },
+      description: "Input value",
+    },
+    label: {
+      control: { type: "text" },
+      description: "Text that appears above the input field",
+    },
+    isRequired: {
+      control: "boolean",
+      description: "Is this input required?",
+    },
+    isDisabled: {
+      control: "boolean",
+      description: "Is the input disabled?",
+    },
+    isInvalid: {
+      control: "boolean",
+      description: "Is the user's input valid?",
+    },
+    isReadOnly: {
+      control: "boolean",
+      description: "Locks the field to its current value",
+    },
+    minLength: {
+      control: "number",
+      description: "Minimum character length for this input",
+    },
+    maxLength: {
+      control: "number",
+      description: "Maximum character length for this input",
+    },
+  },
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TextAreaTemplate: Story = {
+  args: {
+    label: "Label",
+    description: "Optional description or helper text",
+  },
+};
