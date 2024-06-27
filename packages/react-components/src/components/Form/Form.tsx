2a2a0db118e74a63b9259093a91f2d59ea143476
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-06-27 15:43:28 -0700

    basic Form component scaffold

---
+++ packages/react-components/src/components/Form/Form.tsx
@@ -0,0 +1,12 @@
+import {
+  Form as ReactAriaForm,
+  FormProps as ReactAriaFormProps,
+} from "react-aria-components";
+
+import "./Form.css";
+
+export interface FormProps extends ReactAriaFormProps {}
+
+export default function Form({ ...props }: FormProps) {
+  return <ReactAriaForm className={`bcds-react-aria-Form`} {...props} />;
+}
