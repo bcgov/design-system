0b8bc37b0f6f5d972e9a64ba5683d6e474657786
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-07-16 14:50:30 -0700

    Add work in progress TextArea maxLength counter idea

--- packages/react-components/src/pages/TextArea/TextArea.tsx
+++ packages/react-components/src/pages/TextArea/TextArea.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 import { TextArea } from "@/components";
 
 export default function TextAreaPage() {
+  const [text, setText] = useState("initial text state");
+
+  function handleTextChange(text: string) {
+    console.log("new text value: ", text);
+
+    setText(text);
+  }
+
   return (
     <>
       <h2>TextArea</h2>
-      <TextArea />
+      <div>
+        <p>
+          <code>text</code>: {text}{" "}
+        </p>
+        <TextArea
+          label={"Controlled"}
+          value={text}
+          onChange={handleTextChange}
+          maxLength={500}
+        />
+      </div>
+      <div>
+        <TextArea label={"Uncontrolled"} maxLength={500} defaultValue="Hello" />
+      </div>
     </>
   );
 }
