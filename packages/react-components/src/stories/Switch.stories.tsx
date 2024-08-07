6652508f7a031d1a8ed3daa8de79a47f2724239b
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-07 10:24:56 -0700

    styling and storybook expansion

--- packages/react-components/src/stories/Switch.stories.tsx
+++ packages/react-components/src/stories/Switch.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Switch } from "../components";
+import { SwitchProps } from "@/components/Switch";
 
 const meta = {
   title: "Components/Switch/Switch",
@@ -8,7 +9,21 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  argTypes: {},
+  argTypes: {
+    labelPosition: {
+      options: ["left", "right"],
+      control: { type: "radio" },
+      description: "Sets the position of the text label",
+    },
+    isDisabled: {
+      control: "boolean",
+      description: "Disables the switch",
+    },
+    isReadOnly: {
+      control: "boolean",
+      description: "Sets the switch to read-only",
+    },
+  },
 } satisfies Meta<typeof Switch>;
 
 export default meta;
@@ -16,6 +31,32 @@ type Story = StoryObj<typeof meta>;
 
 export const SwitchTemplate: Story = {
   args: {
-    children: "Label",
+    children: "Label text",
+    labelPosition: "right",
+  },
+  render: ({ ...args }: SwitchProps) => <Switch {...args} />,
+};
+
+export const LabelReversed: Story = {
+  ...SwitchTemplate,
+  args: {
+    children: "Label position reversed",
+    labelPosition: "left",
+  },
+};
+
+export const DisabledSwitch: Story = {
+  ...SwitchTemplate,
+  args: {
+    children: "Disabled switch",
+    isDisabled: true,
+  },
+};
+
+export const ReadOnlySwitch: Story = {
+  ...SwitchTemplate,
+  args: {
+    children: "Read-only switch",
+    isReadOnly: true,
   },
 };
