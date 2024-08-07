a0f6be6720831c4eefebec09dc2888d40fbfc9c7
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-07 13:39:24 -0700

    clean up SwitchPage on vite

--- packages/react-components/src/pages/Switch/Switch.tsx
+++ packages/react-components/src/pages/Switch/Switch.tsx
@@ -3,7 +3,21 @@ import { Switch } from "@/components";
 export default function SwitchPage() {
   return (
     <>
-      <Switch>Label</Switch>
+      <h2>Switch</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "var(--layout-margin-medium",
+        }}
+      >
+        <Switch>Label</Switch>
+        <Switch labelPosition="left">Reversed label position</Switch>
+        <Switch isDisabled>Disabled switch</Switch>
+        <Switch labelPosition="left" defaultSelected>
+          Switch on by default
+        </Switch>
+      </div>
     </>
   );
 }
