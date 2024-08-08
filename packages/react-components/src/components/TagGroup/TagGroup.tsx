0d2c01fd94a2524e63de2dc6e8a4fec85ad90a38
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-08 09:08:06 -0700

    fix icon import in TagGroup from #429

--- packages/react-components/src/components/TagGroup/TagGroup.tsx
+++ packages/react-components/src/components/TagGroup/TagGroup.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-aria-components";
 
 import "./TagGroup.css";
-import SvgExclamationIcon from "../SvgExclamationIcon";
+import SvgExclamationIcon from "../Icons/SvgExclamationIcon";
 
 export interface TagGroupProps extends ReactAriaTagGroupProps {
   /**
