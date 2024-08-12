814d46972cbb74f17e135540899229b580562e23
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-12 11:30:00 -0700

    fleshing out component

--- packages/react-components/src/components/ButtonGroup/ButtonGroup.tsx
+++ packages/react-components/src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,12 +1,19 @@
 import "./ButtonGroup.css";
 
 export interface ButtonGroupProps extends React.PropsWithChildren {
+  /* Sets layout of button group */
   orientation?: "horizontal" | "vertical";
+  /* Semantic label for button group */
+  "aria-label"?: string | undefined;
 }
 
-export default function ButtonGroup({ children, ...props }: ButtonGroupProps) {
+export default function ButtonGroup({
+  orientation,
+  children,
+  ...props
+}: ButtonGroupProps) {
   return (
-    <div className="bcds-ButtonGroup" {...props}>
+    <div className={`bcds-ButtonGroup ${orientation}`} role="group" {...props}>
       {children}
     </div>
   );
--- packages/react-components/src/stories/ButtonGroup.stories.tsx
+++ packages/react-components/src/stories/ButtonGroup.stories.tsx
@@ -4,18 +4,47 @@ import { Button, ButtonGroup } from "../components";
 import { ButtonGroupProps } from "@/components/ButtonGroup";
 
 const meta = {
-  title: "Utility/ButtonGroup",
+  title: "Utility/Button group",
   component: ButtonGroup,
   parameters: {
     layout: "centered",
   },
-  argTypes: {},
+  argTypes: {
+    orientation: {
+      options: ["horizontal", "vertical"],
+      control: { type: "radio" },
+      description: "Layout of button group",
+    },
+    "aria-label": {
+      control: { type: "text" },
+      description: "Semantic label for the button group",
+    },
+  },
 } satisfies Meta<typeof ButtonGroup>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const ButtonGroupTemplate: Story = {
-  args: {},
+  args: {
+    orientation: "horizontal",
+    children: [
+      <Button variant="primary">Button 1</Button>,
+      <Button variant="secondary">Button 2</Button>,
+      <Button variant="secondary">Button 3</Button>,
+    ],
+  },
   render: ({ ...args }: ButtonGroupProps) => <ButtonGroup {...args} />,
+};
+
+export const VerticalButtonGroup: Story = {
+  ...ButtonGroupTemplate,
+  args: {
+    orientation: "vertical",
+    children: [
+      <Button variant="primary">Button 1</Button>,
+      <Button variant="secondary">Button 2</Button>,
+      <Button variant="secondary">Button 3</Button>,
+    ],
+  },
 };
