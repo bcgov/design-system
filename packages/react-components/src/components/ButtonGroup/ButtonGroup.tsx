da4789f4d20915d0e33458170b719b3db46c3d52
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-12 11:50:40 -0700

    add alignment prop and styles

--- packages/react-components/src/components/ButtonGroup/ButtonGroup.tsx
+++ packages/react-components/src/components/ButtonGroup/ButtonGroup.tsx
@@ -3,17 +3,24 @@ import "./ButtonGroup.css";
 export interface ButtonGroupProps extends React.PropsWithChildren {
   /* Sets layout of button group */
   orientation?: "horizontal" | "vertical";
+  /* Sets alignment of button group */
+  alignment?: "start" | "end";
   /* Semantic label for button group */
   "aria-label"?: string | undefined;
 }
 
 export default function ButtonGroup({
   orientation,
+  alignment,
   children,
   ...props
 }: ButtonGroupProps) {
   return (
-    <div className={`bcds-ButtonGroup ${orientation}`} role="group" {...props}>
+    <div
+      className={`bcds-ButtonGroup ${orientation} ${alignment}`}
+      role="group"
+      {...props}
+    >
       {children}
     </div>
   );
