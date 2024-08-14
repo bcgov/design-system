b0858a87c1e2e843f821ae3ac18708dab9afb886
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-14 16:39:49 -0700

    roughing out CheckboxGroup

--- packages/react-components/src/components/CheckboxGroup/CheckboxGroup.tsx
+++ packages/react-components/src/components/CheckboxGroup/CheckboxGroup.tsx
@@ -1,17 +1,52 @@
 import {
   CheckboxGroup as ReactAriaCheckboxGroup,
-  CheckboxGroupProps as ReactAriaCheckboxGroupProps,
+  FieldError,
+  Label,
+  Text,
 } from "react-aria-components";
 
-import { Checkbox } from "@/components";
+import type {
+  CheckboxGroupProps as ReactAriaCheckboxGroupProps,
+  ValidationResult,
+} from "react-aria-components";
 
-export interface CheckboxGroupProps extends ReactAriaCheckboxGroupProps {}
+export interface CheckboxGroupProps extends ReactAriaCheckboxGroupProps {
+  children?: React.ReactNode;
+  /* Group label */
+  label?: string;
+  /* Group description */
+  description?: string;
+  /* Error message */
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
 
-export default function CheckboxGroup({ ...props }: CheckboxGroupProps) {
+export default function CheckboxGroup({
+  label,
+  description,
+  errorMessage,
+  children,
+  ...props
+}: CheckboxGroupProps) {
   return (
     <ReactAriaCheckboxGroup
       className="bcds-react-aria-CheckboxGroup"
       {...props}
-    ></ReactAriaCheckboxGroup>
+    >
+      {label && (
+        <Label className="bcds-react-aria-CheckboxGroup--label">{label}</Label>
+      )}
+      {children}
+      {description && (
+        <Text
+          slot="description"
+          className="bcds-react-aria-CheckboxGroup--description"
+        >
+          {description}
+        </Text>
+      )}
+      <FieldError className="bcds-react-aria-CheckboxGroup--error">
+        {errorMessage}
+      </FieldError>
+    </ReactAriaCheckboxGroup>
   );
 }
