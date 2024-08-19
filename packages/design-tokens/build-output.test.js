12c5f8fa1721122ce80514bdc49cc66a11a9a706
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-08-19 13:24:30 -0700

    Test for build removes contents of build directory before script is run

--- packages/design-tokens/build-output.test.js
+++ packages/design-tokens/build-output.test.js
@@ -5,16 +5,20 @@ import assert from "node:assert";
 test("build-output.js should complete without errors", async () => {
   try {
     await new Promise((resolve, reject) => {
-      // Execute the build script using Node.js
-      exec("node build-output.js", (error, stdout, stderr) => {
-        // If there was an error, the test should fail
-        if (error) {
-          console.error("Error executing build-output.js:", stderr);
-          return reject(new Error("build-output.js threw an error"));
+      // Execute the `build` script from package.json, removing the build
+      // directory contents before running build-output.js.
+      exec(
+        "rm -rf build/cjs* && rm -rf build/css* && rm -rf build/js* && node build-output.js",
+        (error, stdout, stderr) => {
+          // If there was an error, the test should fail.
+          if (error) {
+            console.error("Error executing build-output.js:", stderr);
+            return reject(new Error("build-output.js threw an error"));
+          }
+          console.log("build-output.js executed successfully:", stdout);
+          resolve();
         }
-        console.log("build-output.js executed successfully:", stdout);
-        resolve();
-      });
+      );
     });
     assert.ok(true);
   } catch (error) {
