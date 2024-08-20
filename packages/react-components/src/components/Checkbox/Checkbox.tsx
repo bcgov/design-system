767d8a1c244520cfe170706ceed476a00a98509d
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-20 10:14:45 -0700

    fleshing out checkbox stories and docs

--- packages/react-components/src/components/Checkbox/Checkbox.tsx
+++ packages/react-components/src/components/Checkbox/Checkbox.tsx
@@ -10,17 +10,26 @@ import "./Checkbox.css";
 import React from "react";
 
 export interface CheckboxProps extends ReactAriaCheckboxProps {
+  /* Unique identifier for checkbox */
+  value?: string;
+  /* Whether a checkbox is selected by default */
+  defaultSelected?: boolean;
+  /* Used to set label text */
   children: React.ReactNode;
 }
 
-export default function Checkbox({ children, ...props }: CheckboxProps) {
+export default function Checkbox({ value, children, ...props }: CheckboxProps) {
   return (
-    <ReactAriaCheckbox className="bcds-react-aria-Checkbox" {...props}>
+    <ReactAriaCheckbox
+      className="bcds-react-aria-Checkbox"
+      value={value}
+      {...props}
+    >
       {({ isRequired, isSelected, isIndeterminate }) => (
         <>
           <div className="checkbox">
-            {isSelected && <SvgCheckIcon />}
-            {isIndeterminate && !isSelected && <SvgDashIcon />}
+            {isSelected && !isIndeterminate && <SvgCheckIcon />}
+            {isIndeterminate && <SvgDashIcon />}
           </div>
           <span className="label">
             {children} {isRequired && "(required)"}
--- packages/react-components/src/stories/Checkbox.stories.tsx
+++ packages/react-components/src/stories/Checkbox.stories.tsx
@@ -9,7 +9,36 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  argTypes: {},
+  argTypes: {
+    value: {
+      control: { type: "text" },
+      description: "Unique identifier, used for validation and data submission",
+    },
+    children: {
+      control: { type: "object" },
+      description: "Text label",
+    },
+    defaultSelected: {
+      control: { type: "boolean" },
+      description: "Whether a checkbox is selected by default",
+    },
+    isRequired: {
+      control: { type: "boolean" },
+      description: "Whether a checkbox is mandatory",
+    },
+    isIndeterminate: {
+      control: { type: "boolean" },
+      description: "Locks a checkbox to an 'inderminate' state",
+    },
+    isDisabled: {
+      control: { type: "boolean" },
+      description: "Disables a checkbox",
+    },
+    isReadOnly: {
+      control: { type: "boolean" },
+      descriptipn: "Locks a checkbox to its current state",
+    },
+  },
 } satisfies Meta<typeof Checkbox>;
 
 export default meta;
@@ -18,4 +47,40 @@ type Story = StoryObj<typeof meta>;
 export const CheckboxTemplate: Story = {
   args: { children: "This is a checkbox label" },
   render: ({ ...args }: CheckboxProps) => <Checkbox {...args} />,
+};
+
+export const DefaultSelectedCheckbox: Story = {
+  args: {
+    children: "This checkbox is selected by default",
+    defaultSelected: true,
+  },
+};
+
+export const RequiredCheckbox: Story = {
+  args: {
+    children: "This checkbox is mandatory",
+    isRequired: true,
+  },
+};
+
+export const DisabledCheckbox: Story = {
+  args: {
+    children: "This checkbox is disabled",
+    isDisabled: true,
+  },
+};
+
+export const ReadOnlyCheckbox: Story = {
+  args: {
+    children: "This checkbox is set to read-only",
+    isSelected: true,
+    isReadOnly: true,
+  },
+};
+
+export const IndeterminateCheckbox: Story = {
+  args: {
+    children: "This checkbox is neither selected nor deselected",
+    isIndeterminate: true,
+  },
 };
