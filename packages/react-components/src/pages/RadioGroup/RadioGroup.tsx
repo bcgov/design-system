c77cb89f3a39d1aeac1431bbb039dfb889d6c2c9
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-26 14:26:51 -0700

    adding react-grid-system for testing

--- packages/react-components/src/pages/RadioGroup/RadioGroup.tsx
+++ packages/react-components/src/pages/RadioGroup/RadioGroup.tsx
@@ -1,37 +1,54 @@
 import { RadioGroup, Radio } from "@/components";
 
+import { Container, Col, Row } from "react-grid-system";
+
 export default function RadioGroupPage() {
   return (
     <>
-      <h2>RadioGroup</h2>
-      <h3>Required RadioGroup</h3>
-      <RadioGroup
-        label="Text label"
-        description="Additional description or helper text"
-        isRequired
-      >
-        <Radio value="1">Option 1</Radio>
-        <Radio value="2">Option 2</Radio>
-        <Radio value="3">Option 3</Radio>
-        <Radio value="4">Option 4</Radio>
-      </RadioGroup>
-      <h3>RadioGroup with default value</h3>
-      <RadioGroup label="This radio group has a default value" defaultValue="3">
-        <Radio value="1">Option 1</Radio>
-        <Radio value="2">Option 2</Radio>
-        <Radio value="3">Option 3</Radio>
-        <Radio value="4">Option 4</Radio>
-      </RadioGroup>
-      <h3>Horizontal RadioGroup</h3>
-      <RadioGroup
-        label="This is a horizontal radio group"
-        orientation="horizontal"
-      >
-        <Radio value="1">Option 1</Radio>
-        <Radio value="2">Option 2</Radio>
-        <Radio value="3">Option 3</Radio>
-        <Radio value="4">Option 4</Radio>
-      </RadioGroup>
+      <Container>
+        <Row>
+          <h2>RadioGroup</h2>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Required RadioGroup</h3>
+            <RadioGroup
+              label="Text label"
+              description="Additional description or helper text"
+              isRequired
+            >
+              <Radio value="1">Option 1</Radio>
+              <Radio value="2">Option 2</Radio>
+              <Radio value="3">Option 3</Radio>
+              <Radio value="4">Option 4</Radio>
+            </RadioGroup>
+          </Col>
+          <Col>
+            <h3>RadioGroup with default value</h3>
+            <RadioGroup
+              label="This radio group has a default value"
+              defaultValue="3"
+            >
+              <Radio value="1">Option 1</Radio>
+              <Radio value="2">Option 2</Radio>
+              <Radio value="3">Option 3</Radio>
+              <Radio value="4">Option 4</Radio>
+            </RadioGroup>
+          </Col>
+          <Col>
+            <h3>Horizontal RadioGroup</h3>
+            <RadioGroup
+              label="This is a horizontal radio group"
+              orientation="horizontal"
+            >
+              <Radio value="1">Option 1</Radio>
+              <Radio value="2">Option 2</Radio>
+              <Radio value="3">Option 3</Radio>
+              <Radio value="4">Option 4</Radio>
+            </RadioGroup>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
