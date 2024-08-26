03e881de6c11ea835e20e0c86371766a0318fa36
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-08-26 13:40:48 -0700

    flesh out kitchen sink examples

--- packages/react-components/src/pages/RadioGroup/RadioGroup.tsx
+++ packages/react-components/src/pages/RadioGroup/RadioGroup.tsx
@@ -4,9 +4,33 @@ export default function RadioGroupPage() {
   return (
     <>
       <h2>RadioGroup</h2>
-      <RadioGroup label="Text label" isRequired>
+      <h3>Required RadioGroup</h3>
+      <RadioGroup
+        label="Text label"
+        description="Additional description or helper text"
+        isRequired
+      >
         <Radio value="1">Option 1</Radio>
         <Radio value="2">Option 2</Radio>
+        <Radio value="3">Option 3</Radio>
+        <Radio value="4">Option 4</Radio>
+      </RadioGroup>
+      <h3>RadioGroup with default value</h3>
+      <RadioGroup label="This radio group has a default value" defaultValue="3">
+        <Radio value="1">Option 1</Radio>
+        <Radio value="2">Option 2</Radio>
+        <Radio value="3">Option 3</Radio>
+        <Radio value="4">Option 4</Radio>
+      </RadioGroup>
+      <h3>Horizontal RadioGroup</h3>
+      <RadioGroup
+        label="This is a horizontal radio group"
+        orientation="horizontal"
+      >
+        <Radio value="1">Option 1</Radio>
+        <Radio value="2">Option 2</Radio>
+        <Radio value="3">Option 3</Radio>
+        <Radio value="4">Option 4</Radio>
       </RadioGroup>
     </>
   );
