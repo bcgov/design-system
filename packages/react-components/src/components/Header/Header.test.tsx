f3b36c2e3a7e1ff721a670d6cd759e85464329e1
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-08-27 13:55:12 -0700

    Add failing test for Header

---
+++ packages/react-components/src/components/Header/Header.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Header from "./Header";
+
+describe("Header", () => {
+  render(<Header />);
+
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/banner_role
+  const bannerElements = screen.getAllByRole("banner");
+
+  it("renders an HTML header component", () => {
+    expect(bannerElements).toHaveLength(1);
+    expect(bannerElements[0].tagName).toBe("HEADER");
+  });
+});
