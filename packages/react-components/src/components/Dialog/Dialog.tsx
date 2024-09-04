a99a037714737dcdc0b8343e32f4454ab54b0327
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-09-04 16:37:31 -0700

    stripping Dialog down to the studs

--- packages/react-components/src/components/Dialog/Dialog.tsx
+++ packages/react-components/src/components/Dialog/Dialog.tsx
@@ -5,62 +5,34 @@ import {
 } from "react-aria-components";
 
 import Button from "../Button";
-import ButtonGroup from "../ButtonGroup";
 import SvgCloseIcon from "../Icons/SvgCloseIcon";
 
 import "./Dialog.css";
-import React from "react";
 
 export interface DialogProps extends ReactAriaDialogProps {
-  title?: string;
-  description?: string;
-  buttons?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-export default function Dialog({
-  buttons,
-  children,
-  description,
-  title,
-  ...props
-}: DialogProps) {
+export default function Dialog({ children, ...props }: DialogProps) {
   return (
     <ReactAriaDialog className="bcds-react-aria-Dialog" {...props}>
       {({ close }) => (
         <>
-          {children ? (
-            children
-          ) : (
-            <>
-              <div className="bcds-react-aria-Dialog--Header">
-                {title && (
-                  <div className="bcds-react-aria-Dialog--Title">{title}</div>
-                )}
-                <div className="bcds-react-aria-Dialog--closeIcon">
-                  <Button
-                    variant="tertiary"
-                    isIconButton
-                    size="small"
-                    aria-label="Close"
-                    type="button"
-                    onPress={close}
-                  >
-                    <SvgCloseIcon />
-                  </Button>
-                </div>
-              </div>
-              {description && (
-                <div className="bcds-react-aria-Dialog--Description">
-                  {description}
-                </div>
-              )}
-              {buttons && (
-                <ButtonGroup alignment="end" orientation="horizontal">
-                  {buttons}
-                </ButtonGroup>
-              )}
-            </>
-          )}
+          <div className="bcds-react-aria-Dialog--Header">
+            <div className="bcds-react-aria-Dialog--closeIcon">
+              <Button
+                variant="tertiary"
+                isIconButton
+                size="small"
+                aria-label="Close"
+                type="button"
+                onPress={close}
+              >
+                <SvgCloseIcon />
+              </Button>
+            </div>
+          </div>
+          <div className="bcds-react-aria-Dialog--Container">{children}</div>
         </>
       )}
     </ReactAriaDialog>
