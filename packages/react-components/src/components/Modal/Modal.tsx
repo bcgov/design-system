b993351249d7f39961e09ce0778b64e945e93c4a
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-09-05 11:38:09 -0700

    expose size controls for Modal container

--- packages/react-components/src/components/Modal/Modal.tsx
+++ packages/react-components/src/components/Modal/Modal.tsx
@@ -6,12 +6,26 @@ import {
 
 import "./Modal.css";
 
-export interface ModalProps extends ReactAriaModalOverlayProps {}
+export interface ModalProps extends ReactAriaModalOverlayProps {
+  /* Sets height of modal container */
+  modalHeight: string | number;
+  /* Sets width of modal container */
+  modalWidth: string | number;
+}
 
-export default function Modal({ children, ...props }: ModalProps) {
+export default function Modal({
+  children,
+  modalHeight = "auto",
+  modalWidth = 600,
+  ...props
+}: ModalProps) {
   return (
     <ReactAriaModalOverlay className="bcds-react-aria-Modal--Overlay">
-      <ReactAriaModal className="bcds-react-aria-Modal--Container" {...props}>
+      <ReactAriaModal
+        className="bcds-react-aria-Modal--Container"
+        style={{ width: modalWidth, height: modalHeight }}
+        {...props}
+      >
         {children}
       </ReactAriaModal>
     </ReactAriaModalOverlay>
