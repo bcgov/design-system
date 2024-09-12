f12768ce097e79cda889ae709c497b5e600fe3ac
Tyler Krys <Tyler.Krys@gov.bc.ca>
2024-09-12 08:40:50 -0700

    Radio exports RadioPrios directly from react-aria-components

--- packages/react-components/src/components/Radio/Radio.tsx
+++ packages/react-components/src/components/Radio/Radio.tsx
@@ -1,11 +1,6 @@
-import {
-  Radio as ReactAriaRadio,
-  RadioProps as ReactAriaRadioProps,
-} from "react-aria-components";
+import { Radio as ReactAriaRadio, RadioProps } from "react-aria-components";
 
 import "./Radio.css";
-
-export interface RadioProps extends ReactAriaRadioProps {}
 
 export default function Radio({ value, children, ...props }: RadioProps) {
   return (
--- packages/react-components/src/components/Radio/index.ts
+++ packages/react-components/src/components/Radio/index.ts
@@ -1,2 +1,2 @@
 export { default } from "./Radio";
-export type { RadioProps } from "./Radio";
+export type { RadioProps } from "react-aria-components";
