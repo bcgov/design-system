f17ce7da8f52d7a06f95fb4df0f29bd8f87a4737
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2024-10-30 11:46:42 -0700

    implement close function with useState on AlertBanner examples

--- packages/react-components/src/pages/AlertBanner/AlertBanner.tsx
+++ packages/react-components/src/pages/AlertBanner/AlertBanner.tsx
@@ -1,16 +1,58 @@
 import { AlertBanner } from "@/components";
 import { SvgChevronDownIcon } from "@/components";
+import { useState } from "react";
+
+interface BannerState {
+  [key: string]: boolean;
+}
 
 export default function AlertBannerPage() {
+  const [banners, setBanners] = useState<BannerState>({
+    banner1: true,
+    banner2: true,
+    banner3: true,
+    banner4: true,
+    banner5: true,
+  });
+
+  const handleClose = (bannerId: string) => {
+    setBanners((prev) => ({
+      ...prev,
+      [bannerId]: false,
+    }));
+  };
   return (
     <>
-      <AlertBanner>This is an alert banner in its default state</AlertBanner>
-      <AlertBanner variant="success">Success state</AlertBanner>
-      <AlertBanner variant="warning">Warning state</AlertBanner>
-      <AlertBanner variant="danger">Danger state</AlertBanner>
-      <AlertBanner variant="dark" customIcon={<SvgChevronDownIcon />}>
-        Dark state with a custom icon
-      </AlertBanner>
+      {banners.banner1 && (
+        <AlertBanner onClose={() => handleClose("banner1")}>
+          This is an alert banner in its default configuration
+        </AlertBanner>
+      )}
+      {banners.banner2 && (
+        <AlertBanner variant="success" onClose={() => handleClose("banner2")}>
+          Success state
+        </AlertBanner>
+      )}
+      {banners.banner3 && (
+        <AlertBanner variant="warning" onClose={() => handleClose("banner3")}>
+          Warning state
+        </AlertBanner>
+      )}
+      {banners.banner4 && (
+        <AlertBanner variant="danger" onClose={() => handleClose("banner4")}>
+          Danger state
+        </AlertBanner>
+      )}
+      {banners.banner5 && (
+        <AlertBanner
+          variant="dark"
+          isCloseable={false}
+          customIcon={<SvgChevronDownIcon />}
+          onClose={() => handleClose("banner5")}
+        >
+          Dark state with a custom icon and close function disabled
+        </AlertBanner>
+      )}
     </>
   );
 }
