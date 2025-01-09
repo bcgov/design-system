9be8e2bc415cefa076e9233a96cc1d7777bd415f
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-01-09 15:27:34 -0800

    refining styling

--- packages/react-components/src/pages/ToggleButtonGroup/ToggleButtonGroup.tsx
+++ packages/react-components/src/pages/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -1,11 +1,20 @@
-import { ToggleButton, ToggleButtonGroup } from "@/components";
+import {
+  ToggleButton,
+  ToggleButtonGroup,
+  SvgCheckCircleIcon,
+  SvgCloseIcon,
+  SvgExclamationCircleIcon,
+} from "@/components";
 
 export default function ToggleButtonGroupPage() {
   return (
     <>
       <h2>Toggle Button Group</h2>
       <ToggleButtonGroup label="This is a text label">
-        <ToggleButton id="1">Button 1</ToggleButton>
+        <ToggleButton id="1">
+          <SvgCheckCircleIcon />
+          Button 1
+        </ToggleButton>
         <ToggleButton id="2">Button 2</ToggleButton>
         <ToggleButton id="3">Button 3</ToggleButton>
       </ToggleButtonGroup>
@@ -25,6 +34,18 @@ export default function ToggleButtonGroupPage() {
           Button 2
         </ToggleButton>
         <ToggleButton id="3">Button 3</ToggleButton>
+      </ToggleButtonGroup>
+      <h3>Icon buttons</h3>
+      <ToggleButtonGroup selectionMode="multiple" label="This is a text label">
+        <ToggleButton id="1">
+          <SvgCheckCircleIcon />
+        </ToggleButton>
+        <ToggleButton id="2">
+          <SvgExclamationCircleIcon />
+        </ToggleButton>
+        <ToggleButton id="3">
+          <SvgCloseIcon />
+        </ToggleButton>
       </ToggleButtonGroup>
     </>
   );
