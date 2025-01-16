f50658502623dbe716d402e41d9c0f47c9435044
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-01-16 14:16:43 -0800

    WIP on Menu stories

---
+++ packages/react-components/src/stories/MenuItem.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  SvgChevronDownIcon,
+} from "../components";
+
+import { MenuItemProps } from "../components/MenuItem";
+
+const meta = {
+  title: "Components/Menu/MenuItem",
+  component: MenuItem,
+  parameters: { layout: "centered" },
+  argTypes: {
+    children: {
+      control: { type: "object" },
+      description: "Populates menu item label",
+    },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "medium"],
+      description: "Sets the label text size",
+    },
+    id: {
+      control: { type: "text" },
+      description: "Unique identifier for the menu item (required)",
+    },
+    href: {
+      control: { type: "text" },
+      description: "A URL to link to",
+    },
+    hrefLang: {
+      control: { type: "text" },
+      description: "Hints at the human language of the linked URL",
+    },
+    rel: {
+      control: { type: "text" },
+      description:
+        "The relationship between the linked resource and the current page",
+    },
+    target: {
+      control: { type: "text" },
+      description: "The target window for the link",
+    },
+    value: {
+      control: { type: "object" },
+      description: "The object value that this item represents",
+    },
+    textValue: {
+      control: { type: "text" },
+      description:
+        "	A string representation of the item's contents, used for features like typeahead",
+    },
+    isDisabled: {
+      control: { type: "boolean" },
+      description: "Whether the menu item is disabled",
+    },
+  },
+} satisfies Meta<typeof MenuItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const MenuItemTemplate: Story = {
+  args: {
+    size: "medium",
+    children: ["Menu item"],
+    id: "1",
+    isDisabled: false,
+  },
+  render: ({ ...args }: MenuItemProps) => (
+    <MenuTrigger>
+      <Button variant="secondary">
+        Open the menu <SvgChevronDownIcon />
+      </Button>
+      <Menu>
+        <MenuItem {...args} />
+      </Menu>
+    </MenuTrigger>
+  ),
+};
