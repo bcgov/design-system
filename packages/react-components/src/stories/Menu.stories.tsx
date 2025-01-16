f6749eae278b3138e5b7068ab50ab37c7a9373a2
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-01-16 11:36:42 -0800

    adding docs and stories for Menu

---
+++ packages/react-components/src/stories/Menu.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  SubmenuTrigger,
+  SvgChevronDownIcon,
+} from "../components";
+import { MenuProps } from "../components/Menu";
+
+const meta = {
+  title: "Components/Menu/Menu",
+  component: Menu,
+  parameters: { layout: "centered" },
+  argTypes: {
+    children: {
+      control: { type: "object" },
+      description: "Expects an array of `MenuItem` components",
+    },
+  },
+} satisfies Meta<typeof Menu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const MenuItems = [
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
+  { id: 4, name: "Item 4" },
+];
+
+export const MenuTemplate: Story = {
+  args: {
+    children: [<MenuItem>Item 1</MenuItem>],
+  },
+  render: ({ ...args }: MenuProps<object>) => (
+    <MenuTrigger>
+      <Button variant="secondary">
+        Open the menu <SvgChevronDownIcon />
+      </Button>
+      <Menu {...args} />
+    </MenuTrigger>
+  ),
+};
+
+export const MenuWithSubmenu: Story = {
+  args: {
+    children: [
+      <MenuItem id="1">Item 1</MenuItem>,
+      <MenuItem id="2">Item 2</MenuItem>,
+      [
+        <SubmenuTrigger>
+          <MenuItem>Submenu</MenuItem>
+          <Menu>
+            <MenuItem id="1">Submenu item 1</MenuItem>
+            <MenuItem id="2">Submenu item 2</MenuItem>
+          </Menu>
+        </SubmenuTrigger>,
+      ],
+    ],
+  },
+  render: ({ ...args }: MenuProps<object>) => (
+    <MenuTrigger>
+      <Button variant="secondary">
+        Open the menu <SvgChevronDownIcon />
+      </Button>
+      <Menu {...args} />
+    </MenuTrigger>
+  ),
+};
+
+export const MenuWithDynamicItems: Story = {
+  ...MenuTemplate.args,
+  args: { items: MenuItems },
+  render: () => (
+    <MenuTrigger>
+      <Button>Open the menu</Button>
+      <Menu items={MenuItems}>
+        {(item) => <MenuItem>{item.name}</MenuItem>}
+      </Menu>
+    </MenuTrigger>
+  ),
+};
