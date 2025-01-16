ece84e1d6f34837f7cd1153e89ee43af6943056a
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-01-16 14:54:05 -0800

    fixing type errors in MenuItem

--- packages/react-components/src/components/MenuItem/MenuItem.tsx
+++ packages/react-components/src/components/MenuItem/MenuItem.tsx
@@ -1,31 +1,43 @@
 import {
   MenuItem as ReactAriaMenuItem,
   MenuItemProps as ReactAriaMenuItemProps,
+  MenuItemRenderProps,
 } from "react-aria-components";
-
+import { ReactNode } from "react";
 import "./MenuItem.css";
 import SvgChevronRightIcon from "../Icons/SvgChevronRightIcon";
 
 export interface MenuItemProps extends ReactAriaMenuItemProps {
   size?: "small" | "medium";
+  children?:
+    | ReactNode
+    | ((
+        props: MenuItemRenderProps & { defaultChildren: ReactNode }
+      ) => ReactNode);
 }
 
 export default function MenuItem({ size = "small", ...props }: MenuItemProps) {
   const textValue =
     props.textValue ||
     (typeof props.children === "string" ? props.children : undefined);
+
   return (
     <ReactAriaMenuItem
       {...props}
       textValue={textValue}
       className={`bcds-react-aria-Menu-Item ${size}`}
     >
-      {({ hasSubmenu }) => (
-        <>
-          {props.children}
-          {hasSubmenu && <SvgChevronRightIcon />}
-        </>
-      )}
+      {(renderProps: MenuItemRenderProps & { defaultChildren: ReactNode }) => {
+        if (typeof props.children === "function") {
+          return props.children(renderProps);
+        }
+        return (
+          <>
+            {props.children}
+            {renderProps.hasSubmenu && <SvgChevronRightIcon />}
+          </>
+        );
+      }}
     </ReactAriaMenuItem>
   );
 }
