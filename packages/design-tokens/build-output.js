faaedb05055d04c6ce2d072ad09abd39caa0dc38
Robert Johnstone <rob@plasticviking.com>
2025-01-17 12:35:58 -0400

    Add SCSS output format

--- packages/design-tokens/build-output.js
+++ packages/design-tokens/build-output.js
@@ -226,6 +226,30 @@ const sd = new StyleDictionary({
         },
       ],
     },
+    scss: {
+      transformGroup: "tokens-studio",
+      transforms: [
+        "bcds/typography/italic",
+        "bcds/color/uppercase",
+        "bcds/css/size/zero",
+        "name/kebab",
+      ],
+      buildPath: "build/scss/",
+      files: [
+        {
+          destination: "variables.scss",
+          format: "scss/map-deep",
+          filter: (t) => (
+            /* strip metadata */
+            !(['$themes', '$metadata'].includes(t))
+          ),
+          options: {
+            "mapName": "bcds",
+            "themeable": false
+          }
+        },
+      ],
+    },
   },
 });
 
