87b4d0f1af2410cf98f8563aedd6f3868c9fd4a7
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-02-03 10:36:06 -0800

    Add Heading and Text components (#510)
    * implement RAC Text
    
    * scaffolding Text docs
    
    * WIP on implementing BCDS typescale
    
    * React Aria library update to 1.5.0
    
    * Add Accordion and AccordionGroup components (#499)
    
    * staging Accordion and AccordionGroup file structure
    
    * bump RAC to 1.4.0
    
    * basic shape of Accordion component
    
    * tweak props import
    
    * add story wireframe for Accordion
    
    * styling accordion
    
    * styling
    
    * additional styling and alternative icon implementation
    
    * refining button styling
    
    * scaffolding AccordionGroup
    
    * add title prop and styling to AccordionGroup
    
    * continuing work on Accordion and AccordionGroup styling
    
    * fix focus state for accordions inside accordiongroup
    
    * fleshing out argTypes
    
    * scaffolding accordion docs
    
    * add examples to vite kitchen sink
    
    * fleshing out examples on vite
    
    * expanding stories and docs
    
    * stories and docs
    
    * typo
    
    * renaming CSS classes to bcds-react-aria-* to match convention
    
    * typo
    
    * add comments to accordion group props
    
    * remove unused CSS class
    
    * bump RAC to 1.5.0
    
    * update RAC imports
    
    * Refactor Select to use ListBoxSection (#504)
    
    * bump RAC to 1.5.0
    
    * replace Section with ListBoxSection
    
    * Revert "bump RAC to 1.5.0"
    
    This reverts commit 75e05e7ae1045a707e7bf73621a6533b07fd5a3f.
    
    * Reapply "bump RAC to 1.5.0"
    
    This reverts commit 0edc443c769193cf05e97596a2e6de850c32562d.
    
    * storybook library update to 8.4.7
    
    * add TextProps, refining styling
    
    * add missing CSS import
    
    * fork Heading styles into new component
    
    * add vite page for dev and testing
    
    * demo content
    
    * add text colour prop
    
    * wireframing storybook docs
    
    * add isUnstyled prop to Heading and Text
    
    * docs and stories
    
    * add color prop to Heading and flesh out docs
    
    * add ARIA attribute to fix accessibility violation
    
    * add explicit default elementType
    
    * add missing elementType declaration
    
    * docs edits
    
    ---------
    
    Co-authored-by: Arora <Supriya.Arora@gov.bc.ca>
    Co-authored-by: Supriya-Arora <101464129+Supriya-Arora@users.noreply.github.com>

---
+++ packages/react-components/src/stories/Text.stories.tsx
@@ -0,0 +1,104 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Text } from "../components";
+import { TextProps } from "../components/Text";
+
+const meta = {
+  title: "Utility/Headings and text/Text",
+  component: Text,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    children: {
+      control: { type: "object" },
+      description: "Populates the component's content",
+    },
+    elementType: {
+      control: { type: "text" },
+      description: "Defines the type of HTML element that Text renders",
+    },
+    color: {
+      control: {
+        type: "select",
+        options: [
+          "primary",
+          "primaryInvert",
+          "secondary",
+          "secondaryInvert",
+          "disabled",
+          "danger",
+        ],
+      },
+    },
+    isUnstyled: {
+      control: { type: "boolean" },
+      description:
+        "Removes CSS class, allowing component to inherit its styles",
+    },
+    className: {
+      control: { type: "text" },
+      description: "Overrides CSS class",
+    },
+  },
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TextTemplate: Story = {
+  args: { elementType: "span", children: ["This is some text"] },
+  render: ({ ...args }: TextProps) => <Text {...args} />,
+};
+
+export const SmallText: Story = {
+  args: { size: "small", children: ["This text uses the small size"] },
+};
+
+export const MediumText: Story = {
+  args: { size: "medium", children: ["This text uses the medium size"] },
+};
+
+export const LargeText: Story = {
+  args: { size: "large", children: ["This text uses the large size"] },
+};
+
+export const UnstyledText: Story = {
+  ...TextTemplate,
+  args: {
+    isUnstyled: true,
+    children: ["This text is unstyled, and can inherit its styling"],
+  },
+};
+
+export const DisabledText: Story = {
+  ...TextTemplate,
+  args: {
+    color: "disabled",
+    children: ["This text uses the 'disabled' colour"],
+  },
+  render: ({ ...args }: TextProps) => <Text {...args} aria-disabled="true" />,
+};
+
+export const DangerText: Story = {
+  ...TextTemplate,
+  args: { color: "danger", children: ["This text uses the 'danger' colour"] },
+};
+
+export const PrimaryInvert: Story = {
+  ...TextTemplate,
+  args: {
+    color: "primaryInvert",
+    children: ["This text uses the inverted primary colour"],
+  },
+  render: ({ ...args }: TextProps) => (
+    <div
+      style={{
+        backgroundColor: "var(--surface-color-background-dark-blue)",
+        padding: "var(--layout-padding-medium)",
+      }}
+    >
+      <Text {...args} />
+    </div>
+  ),
+};
