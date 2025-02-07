57f5618947af32e440a6bbfd7f2bc63afcea4d0d
Rex Yu <rex.yu@gov.bc.ca>
2025-02-07 11:11:03 -0800

    impl storybook and fix bug

--- packages/react-components/src/pages/NumberField/NumberField.tsx
+++ packages/react-components/src/pages/NumberField/NumberField.tsx
@@ -11,7 +11,12 @@ export default function NumberFieldPage() {
   return (
     <>
       <h2>NumberField</h2>
-      <NumberField value={value} label={"Normal mode"} onChange={handleChange}/>
+      <NumberField label={"Normal mode"} onChange={handleChange} formatOptions= {{
+          style: 'currency',
+          currency: 'EUR',
+          currencyDisplay: 'code',
+          currencySign: 'accounting'
+      }}/>
       <NumberField value={value} label={"Error mode"}/>
       <NumberField value={value} label={"Disable mode"} onChange={(num) => handleChange(num)} isDisabled/>
       <NumberField value={value} label={"Read only mode"} onChange={(num) => handleChange(num)} isReadOnly/>
--- packages/react-components/src/stories/NumberField.stories.tsx
+++ packages/react-components/src/stories/NumberField.stories.tsx
@@ -1,18 +1,68 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { NumberField } from "../components";
-import { NumberFieldProps } from "../components/NumberField";
+import {NumberField} from "../components";
 
 const meta = {
   title: "Components/NumberField",
   component: NumberField,
   parameters: {
     layout: "centered",
+    parameters: { controls: { sort: 'requiredFirst' } },
   },
   argTypes: {
-},
+    value: {
+      control: "number",
+      description: "Desire default value to display"
+    },
+    description: {
+      control: "text",
+      description: "Text to show below the component",
+    },
+    formatOptions: {
+      control: "object",
+      description: "Option to format the sign displaying along with the number and All options supported by Intl.NumberFormat"
+    },
+    minValue: {
+      control: "number",
+      description: "Set the minimum value",
+    },
+    maxValue: {
+      control: "number",
+      description: "Set the maximum value",
+    },
+    step: {
+      control: "number",
+      description: "value for the increment/decrement steps each click would take",
+    },
+    isReadOnly: {
+      control: "boolean",
+      description: "Is the NumberField read-only",
+    },
+    isDisabled: {
+      control: "boolean",
+      description: "Is the NumberField disabled",
+    },
+    label: {
+      control: "text",
+      description: "Header of the component",
+    },
+    onChange: {
+      description: "Click/press handler",
+      type: {
+        required: true
+      }
+    },
+  },
   args: {},
 } satisfies Meta<typeof NumberField>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+export const NumberFieldTemplate: Story = {
+  args: {
+    onChange: () => {}
+  },
+  render: ({ ...args }) => <NumberField {...args} />,
+};
+
