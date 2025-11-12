4cda8c0b1a9f46ad546e0275c6fea566b7346f89
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-11-12 14:23:33 -0800

    Revert "remove storybook/test-runner as dependency, migrating to vitest"
    This reverts commit 857cc2d4c97fad3eca63af2e0c60eab3bae3a6f5.

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -3,12 +3,7 @@ import * as tokens from "@bcgov/design-tokens/cjs/index.js";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest"
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {
