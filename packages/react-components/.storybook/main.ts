8e0d81fffe06385a47b7ab5ef31a66b423a449bd
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-11-20 11:51:50 -0800

    Upgrade Storybook to v9 (#549)
    * upgrade storybook to v9 and run automigrations
    
    * downgrade test-runner to 0.19.1

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -3,12 +3,7 @@ import * as tokens from "@bcgov/design-tokens/cjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {
--- packages/react-components/src/stories/Header.stories.tsx
+++ packages/react-components/src/stories/Header.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { Button, Header } from "../components";
 import { HeaderProps } from "../components/Header";
