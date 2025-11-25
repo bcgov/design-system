76af016c4f5261da05c0145e8f530d86640d3579
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-11-24 17:10:07 -0800

    add small placeholder icon

--- packages/react-components/src/pages/Button/ButtonPrimary.tsx
+++ packages/react-components/src/pages/Button/ButtonPrimary.tsx
@@ -26,6 +26,32 @@ const iconPlaceholder = (
   </svg>
 );
 
+const iconPlaceholderSmall = (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="16px/Icon placeholder" clipPath="url(#clip0_717_6325)">
+      <path
+        id="Icon"
+        d="M9.99996 18.3333C14.6023 18.3333 18.3333 14.6023 18.3333 9.99996C18.3333 5.39759 14.6023 1.66663 9.99996 1.66663C5.39759 1.66663 1.66663 5.39759 1.66663 9.99996C1.66663 14.6023 5.39759 18.3333 9.99996 18.3333Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_717_6325">
+        <rect width="20" height="20" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
 function ButtonPrimaryLarge() {
   return (
     <Button size="large" variant="primary">
@@ -137,7 +163,7 @@ function ButtonPrimaryXSmallDisabled() {
 function ButtonPrimaryXSmallIcon() {
   return (
     <Button size="xsmall" variant="primary" isIconButton>
-      {iconPlaceholder}
+      {iconPlaceholderSmall}
     </Button>
   );
 }
@@ -145,7 +171,7 @@ function ButtonPrimaryXSmallIcon() {
 function ButtonPrimaryXSmallIconDisabled() {
   return (
     <Button size="xsmall" variant="primary" isDisabled isIconButton>
-      {iconPlaceholder}
+      {iconPlaceholderSmall}
     </Button>
   );
 }
