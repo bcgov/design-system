cda0453fe9c75061e658f230fcc0164d81ce72ef
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-11-27 13:55:21 -0800

    Add Vitest to Storybook V10 (#561)
    * Add vitest with Storybook's script
    
    * Storybook a11y violation errors fail CI
    
    * Update Storybook test scripts in package.json to use vitest

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -3,7 +3,12 @@ import * as tokens from "@bcgov/design-tokens/cjs/index.js";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-a11y", "@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {
