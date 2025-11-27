46a1d5581717b016bbe39af396e456f3c171e721
Tyler Krys <Tyler.Krys@gov.bc.ca>
2025-11-27 08:27:50 -0800

    Add vitest with Storybook's script

--- packages/react-components/.storybook/preview.ts
+++ packages/react-components/.storybook/preview.ts
@@ -8,22 +8,32 @@ import "./global.css";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+
     docs: {
       theme: bcdsTheme,
       canvas: {
         withToolbar: true,
       },
     },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+
     options: {
       storySort: {
         order: ["Introduction", "Changelog"],
       },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
     },
   },
 };
