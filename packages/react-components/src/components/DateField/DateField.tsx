134a6776da45b72f1bd6f0484c3113f16f747f27
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-12-10 16:15:29 -0800

    roughing out component structure

---
+++ packages/react-components/src/components/DateField/DateField.tsx
@@ -0,0 +1,58 @@
+import {
+  DateField as ReactAriaDateField,
+  DateFieldProps as ReactAriaDateFieldProps,
+  DateValue,
+  DateInput,
+  DateSegment,
+  FieldError,
+  ValidationResult,
+  Label,
+  Text,
+} from "react-aria-components";
+
+import "./DateField.css";
+
+export interface DateFieldProps<T extends DateValue>
+  extends ReactAriaDateFieldProps<T> {
+  size?: "medium" | "small";
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
+
+export default function DateField<T extends DateValue>({
+  size = "medium",
+  label,
+  description,
+  isRequired,
+  errorMessage,
+  ...props
+}: DateFieldProps<T>) {
+  return (
+    <ReactAriaDateField
+      className={`bcds-react-aria-DateField ${size}`}
+      {...props}
+    >
+      <Label className="bcds-react-aria-DateField--Label">
+        {label}
+        {isRequired && (
+          <span className="bcds-react-aria-DateField--Label required">
+            (required)
+          </span>
+        )}
+      </Label>
+      <DateInput>{(segment) => <DateSegment segment={segment} />}</DateInput>
+      {description && (
+        <Text
+          slot="description"
+          className={`bcds-react-aria-DateField--Description`}
+        >
+          {description}
+        </Text>
+      )}
+      <FieldError className="bcds-react-aria-DateField--Error">
+        {errorMessage}
+      </FieldError>
+    </ReactAriaDateField>
+  );
+}
---
+++ packages/react-components/src/components/DateField/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./DateField";
+export type { DateFieldProps } from "./DateField";
