df221b75f3c90fdfed0809c80890e2452ec71d82
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2025-12-16 16:51:47 -0800

    update vitest config and switch "npm test" script to vitest

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -28,6 +28,15 @@ export default defineConfig({
     projects: [
       {
         extends: true,
+        test: {
+          name: "unit",
+          globals: true,
+          environment: "jsdom",
+          include: ["./src/components/**/*.test.{ts,tsx,js,jsx}"],
+        },
+      },
+      {
+        extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
