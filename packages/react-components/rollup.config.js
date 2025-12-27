52b1d57119087374f259b89e799fc5be41cbdcc8
Tyler Krys <tylerkrys@gmail.com>
2025-12-26 23:11:31 -0700

    Bump @rollup/plugin-typescript to latest, refactor Rollup build for split steps

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { dts } from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
@@ -28,16 +27,10 @@ export default [
       resolve(),
       commonjs(),
       typescript({
-        tsconfig: "./tsconfig.json",
+        tsconfig: "./tsconfig.build.json",
         include: ["src/index.ts", "src/components/**/*"],
       }),
       postcss(),
     ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
   },
 ];
