6c7e4ef7c3351d8a6132208fc7ceefaca2356e4e
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-01-13 13:22:14 -0800

    add React Aria logo

--- packages/react-components/src/components/index.ts
+++ packages/react-components/src/components/index.ts
@@ -32,6 +32,7 @@ export { default as SvgChevronUpIcon } from "./Icons/SvgChevronUpIcon";
 export { default as SvgChevronDownIcon } from "./Icons/SvgChevronDownIcon";
 export { default as SvgCloseIcon } from "./Icons/SvgCloseIcon";
 export { default as SvgInfoIcon } from "./Icons/SvgInfoIcon";
+export { default as SvgReactAriaIcon } from "./Icons/SvgReactAriaIcon";
 export { default as SvgTooltipArrowUp } from "./Icons/SvgTooltipArrowUp";
 export { default as Tag } from "./Tag";
 export { default as TagGroup } from "./TagGroup";
