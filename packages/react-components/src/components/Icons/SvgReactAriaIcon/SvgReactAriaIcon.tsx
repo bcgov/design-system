763234b2747c3695f60aaf898c045cb9e949af16
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-01-13 14:31:32 -0800

    POC and styling fixes for RAC icon and Storybook link buttons

--- packages/react-components/src/components/Icons/SvgReactAriaIcon/SvgReactAriaIcon.tsx
+++ packages/react-components/src/components/Icons/SvgReactAriaIcon/SvgReactAriaIcon.tsx
@@ -4,7 +4,7 @@ export default function SvgReactAriaIcon({ id = "react-aria-icon" }) {
       id={id}
       width="20"
       height="20"
-      viewBox="0 0 20 20"
+      viewBox="200 206 800 790"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g>
