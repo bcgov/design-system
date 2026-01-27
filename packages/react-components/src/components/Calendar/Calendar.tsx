97a43309140758b00268b174aad400d5c67aab7e
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-01-27 11:07:40 -0800

    extend Calendar to support multiple CalendarGrids

--- packages/react-components/src/components/Calendar/Calendar.tsx
+++ packages/react-components/src/components/Calendar/Calendar.tsx
@@ -6,6 +6,8 @@ import {
   DateValue,
 } from "react-aria-components";
 
+import { useDateFormatter } from "react-aria";
+
 import "./Calendar.css";
 
 import Button from "../Button";
@@ -15,29 +17,72 @@ import SvgChevronRightIcon from "../Icons/SvgChevronRightIcon";
 
 export type { DateValue };
 
-export default function Calendar({ ...props }: CalendarProps<DateValue>) {
+export default function Calendar({
+  isHeaderHidden = false,
+  visibleDuration = { months: 1 },
+  ...props
+}: CalendarProps<DateValue>) {
+  const monthFormatter = useDateFormatter({
+    month: "long",
+    year: "numeric",
+    timeZone: "UTC",
+  });
+
   return (
-    <ReactAriaCalendar className="bcds-react-aria-Calendar" {...props}>
-      <header className="bcds-react-aria-Calendar--Header">
-        <Button slot="previous" size="xsmall" variant="tertiary" isIconButton>
-          <SvgChevronLeftIcon />
-        </Button>
-        <Heading />
-        <Button slot="next" size="xsmall" variant="tertiary" isIconButton>
-          <SvgChevronRightIcon />
-        </Button>
-      </header>
-      <ReactAriaCalendarGrid
-        className={`bcds-react-aria-Calendar--Grid`}
-        weekdayStyle="short"
-      >
-        {(date) => (
-          <ReactAriaCalendarCell
-            className="bcds-react-aria-Calendar--Cell"
-            date={date}
-          />
-        )}
-      </ReactAriaCalendarGrid>
+    <ReactAriaCalendar
+      className="bcds-react-aria-Calendar"
+      visibleDuration={visibleDuration}
+      {...props}
+    >
+      {({ state }) =>
+        [...Array(visibleDuration.months).keys()].map((i) => (
+          <div key={i} className="bcds-react-aria-Calendar--GridContainer">
+            {!isHeaderHidden && (
+              <header className="bcds-react-aria-Calendar--Header">
+                {i === 0 && (
+                  <Button
+                    slot="previous"
+                    size="xsmall"
+                    variant="tertiary"
+                    isIconButton
+                  >
+                    <SvgChevronLeftIcon />
+                  </Button>
+                )}
+                <Heading>
+                  {monthFormatter.format(
+                    state.visibleRange.start
+                      .add({ months: i })
+                      .toDate(state.timeZone),
+                  )}
+                </Heading>
+                {i === (visibleDuration.months ?? 1) - 1 && (
+                  <Button
+                    slot="next"
+                    size="xsmall"
+                    variant="tertiary"
+                    isIconButton
+                  >
+                    <SvgChevronRightIcon />
+                  </Button>
+                )}
+              </header>
+            )}
+            <ReactAriaCalendarGrid
+              offset={{ months: i }}
+              className={`bcds-react-aria-Calendar--Grid`}
+              weekdayStyle="short"
+            >
+              {(date) => (
+                <ReactAriaCalendarCell
+                  className="bcds-react-aria-Calendar--Cell"
+                  date={date}
+                />
+              )}
+            </ReactAriaCalendarGrid>
+          </div>
+        ))
+      }
     </ReactAriaCalendar>
   );
 }
