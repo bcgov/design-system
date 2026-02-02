0952526c11a7d91d4d22935a18b782c20c06a5d8
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-02-02 11:24:43 -0800

    Add ToggleButton and ToggleButtonGroup components (#581)
    * add ToggleButton and ToggleButtonGroup components
    
    * add ToggleButton and ToggleButtonGroup stories
    
    * refining styling
    
    * tweak flex and wrap behaviour
    
    * update story categorisation
    
    * fix background color in focus state
    
    * remove redundant styling from ToggleButtonGroup
    
    * add SelectionIndicator and new styling to support animation
    
    * animate ToggleButtonGroup selection change
    
    * fix children typing
    
    * update story categorization to match new rubric
    
    * refine focus state
    
    * remove redundant typing
    
    * add keys to ToggleButton elements
    
    * stories cleanup
    
    * refine styling when ToggleButtonGroup wraps
    
    * fix Storybook foldering
    
    * refining flex styling and stories
    
    * simplify wrapped example story
    
    * refining flex styling
    
    * fix failing test
    
    * update state styling to match design spec
    
    * simplify transition animation
    
    * use outline instead of border for focus ring
    
    * remove SelectionIndicator and simplify animation

---
+++ packages/react-components/src/stories/ToggleButton.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { ToggleButton, SvgCheckCircleIcon } from "../components";
+import { ToggleButtonProps } from "../components/ToggleButton";
+
+const meta = {
+  title: "Inputs and controls/Toggle buttons/Toggle button",
+  component: ToggleButton,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    id: {
+      control: { type: "text" },
+      description: "Unique identifier for the button (required)",
+    },
+    children: {
+      control: { type: "object" },
+      description: "Populates button text",
+    },
+    isIconButton: {
+      control: { type: "boolean" },
+      description: "Must be set if not using a text label",
+    },
+    "aria-label": {
+      control: { type: "text" },
+      description: "Must be set if not using a text label",
+    },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "medium"],
+      description: "Sets button size",
+    },
+    isDisabled: {
+      control: { type: "boolean" },
+      description: "Whether a button is disabled",
+    },
+    autoFocus: {
+      control: { type: "boolean" },
+      description: "Whether the element should receive focus on render",
+    },
+    isSelected: {
+      control: { type: "boolean" },
+      description: "Whether the element should be selected (controlled)",
+    },
+    defaultSelected: {
+      control: { type: "boolean" },
+      description: "Whether the element should be selected (uncontrolled)",
+    },
+  },
+} satisfies Meta<typeof ToggleButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ToggleButtonTemplate: Story = {
+  args: { children: ["Button text"], id: "1", isDisabled: false },
+  render: ({ ...args }: ToggleButtonProps) => <ToggleButton {...args} />,
+};
+
+export const SmallToggleButton: Story = {
+  ...ToggleButtonTemplate,
+  args: {
+    children: [<SvgCheckCircleIcon />, "Button with icon"],
+    size: "small",
+  },
+};
+
+export const ToggleButtonWithIcon: Story = {
+  ...ToggleButtonTemplate,
+  args: {
+    children: [<SvgCheckCircleIcon />, "Button with icon"],
+  },
+};
+
+export const IconOnlyToggleButton: Story = {
+  ...ToggleButtonTemplate,
+  args: {
+    isIconButton: true,
+    "aria-label": "Confirm",
+    children: [<SvgCheckCircleIcon />],
+  },
+};
+
+export const DefaultSelectedToggleButton: Story = {
+  ...ToggleButtonTemplate,
+  args: {
+    children: ["This button is selected by default"],
+    defaultSelected: true,
+  },
+};
+
+export const DisabledToggleButton: Story = {
+  ...ToggleButtonTemplate,
+  args: { children: ["This button is disabled"], isDisabled: true },
+};
