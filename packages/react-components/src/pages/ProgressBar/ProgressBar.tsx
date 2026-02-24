a33245850e7d0f91e10088e7031c400e534bfa83
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-02-24 14:02:13 -0800

    update examples on Vite app

--- packages/react-components/src/pages/ProgressBar/ProgressBar.tsx
+++ packages/react-components/src/pages/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,4 @@
-import { ProgressBar } from "@/components";
+import { ProgressBar, ProgressCircle } from "@/components";
 
 export default function ProgressBarPage() {
   return (
@@ -7,32 +7,16 @@ export default function ProgressBarPage() {
       <h3>Bar</h3>
       <div className="row">
         <div className="col">
-          <ProgressBar
-            format="bar"
-            size="small"
-            valueLabel="Small"
-            value={25}
-          />
+          <ProgressBar size="small" valueLabel="Small" value={25} />
+        </div>
+        <div className="col">
+          <ProgressBar size="medium" valueLabel="Medium" value={50} />
+        </div>
+        <div className="col">
+          <ProgressBar size="large" valueLabel="Large" value={75} />
         </div>
         <div className="col">
           <ProgressBar
-            format="bar"
-            size="medium"
-            valueLabel="Medium"
-            value={50}
-          />
-        </div>
-        <div className="col">
-          <ProgressBar
-            format="bar"
-            size="large"
-            valueLabel="Large"
-            value={75}
-          />
-        </div>
-        <div className="col">
-          <ProgressBar
-            format="bar"
             size="large"
             valueLabel="Indeterminate"
             isIndeterminate
@@ -42,32 +26,16 @@ export default function ProgressBarPage() {
       <h3>Circle</h3>
       <div className="row">
         <div className="col">
-          <ProgressBar
-            format="circle"
-            size="small"
-            valueLabel="Small"
-            value={25}
-          />
+          <ProgressCircle size="small" valueLabel="Small" value={25} />
         </div>
         <div className="col">
-          <ProgressBar
-            format="circle"
-            size="medium"
-            valueLabel="Medium"
-            value={50}
-          />
+          <ProgressCircle size="medium" valueLabel="Medium" value={50} />
         </div>
         <div className="col">
-          <ProgressBar
-            format="circle"
-            size="large"
-            valueLabel="Large"
-            value={75}
-          />
+          <ProgressCircle size="large" valueLabel="Large" value={75} />
         </div>
         <div className="col">
-          <ProgressBar
-            format="circle"
+          <ProgressCircle
             size="large"
             valueLabel="Indeterminate"
             isIndeterminate
