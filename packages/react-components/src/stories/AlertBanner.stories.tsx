e1c629595febbeb52a6ea7081eacf9108a725bb1
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-05-25 09:25:23 -0700

    Add alignment and size props to AlertBanner (#678)
    * add sizing logic to AlertBanner
    
    * add AlertBanner sizing to Storybook
    
    * add alignment prop to AlertBanner
    
    * add alignment controls to Storybook
    
    * rename alignment prop
    
    * add test suite

--- packages/react-components/src/stories/AlertBanner.stories.tsx
+++ packages/react-components/src/stories/AlertBanner.stories.tsx
@@ -14,6 +14,22 @@ const meta = {
       control: { type: "radio" },
       description: "Sets the theme and icon for the alert",
     },
+    size: {
+      options: ["small", "medium"],
+      control: { type: "radio" },
+      description: "Sets the vertical height and font size of the banner",
+      table: {
+        defaultValue: { summary: "medium" },
+      },
+    },
+    layout: {
+      options: ["fixed", "fluid"],
+      control: { type: "radio" },
+      description: "Controls whether banner content is centered or full-width",
+      table: {
+        defaultValue: { summary: "fixed" },
+      },
+    },
     children: {
       control: { type: "object" },
       description: "Populates the content of the alert",
@@ -54,6 +70,22 @@ export const AlertBannerTemplate: Story = {
     onClose: () => alert("onClose()"),
   },
   render: ({ ...args }: AlertBannerProps) => <AlertBanner {...args} />,
+};
+
+export const SmallAlertBanner: Story = {
+  ...AlertBannerTemplate,
+  args: {
+    size: "small",
+    ...AlertBannerTemplate.args,
+  },
+};
+
+export const FullWidthAlertBanner: Story = {
+  ...AlertBannerTemplate,
+  args: {
+    layout: "fluid",
+    ...AlertBannerTemplate.args,
+  },
 };
 
 export const SuccessBanner: Story = {
