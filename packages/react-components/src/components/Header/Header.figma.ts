19860feedb6fffdcb3cddf1aa2cf75c360f63610
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-06-03 14:59:04 -0700

    refactor Code Connect config for Header to integrate Navbar

--- packages/react-components/src/components/Header/Header.figma.ts
+++ packages/react-components/src/components/Header/Header.figma.ts
@@ -8,12 +8,21 @@ const logoImage = figma.selectedInstance
   .getInstanceSwap("Logo")
   ?.executeTemplate().example;
 const children = figma.properties.children(["Header content"]);
+const hasSubheader = figma.selectedInstance.getBoolean("Has subheader");
+const subheaderChildren = figma.selectedInstance.getSlot("Slot 2");
 
 const { renderProp, renderChildren } = figma.helpers.react;
 
 export default {
   id: "Header",
-  imports: ["import { Header } from '@bcgov/design-system-react-components';"],
-  example: figma.code`<Header${renderProp("logoImage", logoImage)}>${renderChildren(children)}</Header>`,
+  imports: [
+    "import { Header, Navbar } from '@bcgov/design-system-react-components';",
+  ],
+  example: hasSubheader
+    ? figma.code`<>
+  <Header${renderProp("logoImage", logoImage)}>${renderChildren(children)}</Header>
+  <Navbar>${renderChildren(subheaderChildren)}</Navbar>
+</>`
+    : figma.code`<Header${renderProp("logoImage", logoImage)}>${renderChildren(children)}</Header>`,
   metadata: { nestable: true },
 };
