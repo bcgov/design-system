5d74c88b44d2cfbbe35369a4358ddf02ae714c88
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-06-15 15:44:19 -0700

    remove redundant Popover

--- packages/react-components/src/App.tsx
+++ packages/react-components/src/App.tsx
@@ -130,13 +130,11 @@ function App() {
           <Button size="small" variant="secondary">
             Menu <SvgMenuIcon />
           </Button>
-          <Popover>
-            <Menu>
-              <MenuItem className="menu-item">Link</MenuItem>
-              <MenuItem className="menu-item">Link</MenuItem>
-              <MenuItem className="menu-item">Link</MenuItem>
-            </Menu>
-          </Popover>
+          <Menu>
+            <MenuItem className="menu-item">Link</MenuItem>
+            <MenuItem className="menu-item">Link</MenuItem>
+            <MenuItem className="menu-item">Link</MenuItem>
+          </Menu>
         </MenuTrigger>
       </Subheader>
       <AlertBannerPage />
