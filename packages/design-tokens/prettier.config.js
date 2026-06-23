ff55cc3e9001b5f2a292515fc596823e277d28c6
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-06-23 13:14:53 -0700

    Add formatter to design-tokens (#753)
    * install and configure Prettier
    
    * test script also removes SCSS build folders
    
    * formatting
    
    * add .prettierignore and remove build and dist folders from formatter scope
    
    * Revert "formatting"
    
    This reverts commit 63d79a3410c0539bd4715ee0ea82cdfc6de81350.

---
+++ packages/design-tokens/prettier.config.js
@@ -0,0 +1,21 @@
+// prettier.config.js
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  // https://prettier.io/docs/options#trailing-commas
+  trailingComma: "es5",
+
+  // https://prettier.io/docs/options#tab-width
+  tabWidth: 2,
+
+  // https://prettier.io/docs/options#semicolons
+  semi: true,
+
+  // https://prettier.io/docs/options#quotes
+  singleQuote: false,
+};
+
+export default config;
