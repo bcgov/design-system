696907b8c9dc87ccaa2acae77ff2bb8a013e8de0
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-06-25 17:44:49 -0700

    add Code Connect mappings for SvgBcLogo and SvgPlaceholderIcon

---
+++ packages/react-components/src/components/Icons/SvgBcLogo/SvgBcLogo.figma.ts
@@ -0,0 +1,14 @@
+// url= https://www.figma.com/design/6BAmnRmL9FXxY2bkkSYiQj/B.C.-Design-System?node-id=3351-1788&t=yPX74R4GVseHsAvd-4
+// source=https://github.com/bcgov/design-system/blob/main/packages/react-components/src/components/Icons/SvgBcLogo/SvgBcLogo.tsx
+// component=BC Logo
+
+import figma from "figma";
+
+export default {
+  id: "BC Logo",
+  imports: [
+    "import { SvgBcLogo } from '@bcgov/design-system-react-components';",
+  ],
+  example: figma.code`<SvgBcLogo />`,
+  metadata: { nestable: true },
+};
---
+++ packages/react-components/src/components/Icons/SvgPlaceholderIcon/SvgPlaceholderIcon.figma.ts
@@ -0,0 +1,14 @@
+// url=https://www.figma.com/design/6BAmnRmL9FXxY2bkkSYiQj/B.C.-Design-System?node-id=14242-12374&t=yPX74R4GVseHsAvd-4
+// source=https://github.com/bcgov/design-system/blob/main/packages/react-components/src/components/Icons/SvgPlaceholderIcon/SvgPlaceholderIcon.tsx
+// component=Placeholder Icon
+
+import figma from "figma";
+
+export default {
+  id: "Placeholder icon",
+  imports: [
+    "import { SvgPlaceholderIcon } from '@bcgov/design-system-react-components';",
+  ],
+  example: figma.code`<SvgPlaceholderIcon />`,
+  metadata: { nestable: true },
+};
