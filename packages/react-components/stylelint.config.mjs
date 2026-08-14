2f363cce6caa66c23ad4ee42e65ffcadac0ac310
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-08-14 13:36:14 -0700

    Add initial stylelint configuration

---
+++ packages/react-components/stylelint.config.mjs
@@ -0,0 +1,39 @@
+export default {
+  extends: "stylelint-config-standard",
+  rules: {
+    "at-rule-no-unknown": true,
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+
+    /**
+     * We prefer to allow comment placement freedom.
+     */
+    "comment-empty-line-before": null,
+
+    /**
+     * TODO:
+     *
+     * We have too many violations of this to turn it on initially, but it would
+     * be a good idea to refactor our existing code and use it.
+     *
+     * See: https://stylelint.io/user-guide/rules/no-descending-specificity
+     */
+    "no-descending-specificity": false,
+    "no-duplicate-at-import-rules": true,
+    "property-no-unknown": true,
+
+    /**
+     * We want to allow freedom here to let CSS authors maximize readability,
+     * grouping similar rules when necessary.
+     */
+    "rule-empty-line-before": null,
+
+    /**
+     * The default for this in `stylelint-config-standard` is kebab case, but we
+     * want to allow mixed case selectors like React Aria Components, ex:
+     * `.react-aria-Button`.
+     */
+    "selector-class-pattern": null,
+    "unit-no-unknown": true,
+  },
+};
