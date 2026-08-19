dc0400bb1f9894d43a93766f5c1bcfaedbcdce62
Marcus Kernohan <135075821+mkernohanbc@users.noreply.github.com>
2026-08-19 16:36:23 -0700

    refactor Radio to use RadioField and RadioButton

--- packages/react-components/src/components/Radio/Radio.tsx
+++ packages/react-components/src/components/Radio/Radio.tsx
@@ -1,11 +1,36 @@
-import { Radio as ReactAriaRadio, RadioProps } from "react-aria-components";
+import {
+  RadioField,
+  RadioButton,
+  RadioFieldProps as ReactAriaRadioFieldProps,
+  Text,
+} from "react-aria-components";
 
 import "./Radio.css";
 
-export default function Radio({ value, children, ...props }: RadioProps) {
+export interface RadioProps extends ReactAriaRadioFieldProps {
+  /* Sets text label for radio button */
+  children?: React.ReactNode;
+  /* Sets optional description text below label */
+  description?: string;
+}
+
+export default function Radio({
+  value,
+  children,
+  description,
+  ...props
+}: RadioProps) {
   return (
-    <ReactAriaRadio className="bcds-react-aria-Radio" value={value} {...props}>
-      {children}
-    </ReactAriaRadio>
+    <RadioField className="bcds-react-aria-Radio" value={value} {...props}>
+      <RadioButton className="bcds-react-aria-Radio--Button">
+        <div className="bcds-react-aria-Radio--Indicator" />
+        <span className="bcds-react-aria-Radio--Label">{children}</span>
+      </RadioButton>
+      {description && (
+        <Text slot="description" className="bcds-react-aria-Radio--Description">
+          {description}
+        </Text>
+      )}
+    </RadioField>
   );
 }
